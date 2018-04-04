bb92adc35cdc874eb70c9f7573dbbf951243069f
Florian <fw@studer-raimann.ch>
2018-04-04 11:34:45 +0200

    Fix require path in dbupdate.php

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -1,6 +1,6 @@
 <#1>
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once 'Customizing/global/plugins/Services/Repository/RepositoryObject/AttendanceList/vendor/autoload.php';
 xaliChecklist::updateDB();
 xaliChecklistEntry::updateDB();
 xaliSetting::updateDB();
@@ -8,7 +8,7 @@ xaliUserStatus::updateDB();
 ?>
 <#2>
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once 'Customizing/global/plugins/Services/Repository/RepositoryObject/AttendanceList/vendor/autoload.php';
 foreach (xaliChecklistEntry::where(array('status' => xaliChecklistEntry::STATUS_ABSENT_EXCUSED))->get() as $entry) {
 	$entry->setStatus(xaliChecklistEntry::STATUS_ABSENT_UNEXCUSED);
 	$entry->update();
@@ -16,7 +16,7 @@ foreach (xaliChecklistEntry::where(array('status' => xaliChecklistEntry::STATUS_
 ?>
 <#3>
 <?php
-require_once __DIR__ . '/vendor/autoload.php';
+require_once 'Customizing/global/plugins/Services/Repository/RepositoryObject/AttendanceList/vendor/autoload.php';
 xaliConfig::updateDB();
 xaliAbsenceReason::updateDB();
 xaliLastReminder::updateDB();
