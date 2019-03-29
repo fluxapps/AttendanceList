ed370a679641755c44a9fa00fcf1a2dfa2e114f4
Florian Wyss <fw@studer-raimann.ch>
2019-03-29 15:07:51 +0100

    Notifications4Plugin

--- cron.php
+++ cron.php
@@ -1,6 +1,7 @@
 <?php
 chdir(substr($_SERVER['SCRIPT_FILENAME'], 0, strpos($_SERVER['SCRIPT_FILENAME'], '/Customizing')));
-require_once 'Customizing/global/plugins/Services/Repository/RepositoryObject/AttendanceList/classes/class.xaliCron.php';
+require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . './../../../UIComponent/UserInterfaceHook/Notifications4Plugins/vendor/autoload.php';
 $cron = new xaliCron($_SERVER['argv']);
 $cron->run();
 $cron->logout();
