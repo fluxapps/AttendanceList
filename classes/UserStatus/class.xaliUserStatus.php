7b77509f077a36140e698adc630c25fe596a8096
mstuder <ms@studer-raimann.ch>
2019-12-13 22:28:45 +0100

    fix percentage

--- classes/UserStatus/class.xaliUserStatus.php
+++ classes/UserStatus/class.xaliUserStatus.php
@@ -331,8 +331,8 @@ class xaliUserStatus extends ActiveRecord {
 			return 0;
 		}
 		return round(
-			$this->getAttendanceStatuses(xaliChecklistEntry::STATUS_PRESENT) / (count($this->getChecklistIds())
-            - $this->getAttendanceStatuses(xaliChecklistEntry::STATUS_NOT_RELEVANT)) * 100
+			$this->getAttendanceStatuses(xaliChecklistEntry::STATUS_PRESENT) / ($this->getAttendanceStatuses(xaliChecklistEntry::STATUS_PRESENT) + $this->getAttendanceStatuses(xaliChecklistEntry::STATUS_ABSENT_EXCUSED)
+            + $this->getAttendanceStatuses(xaliChecklistEntry::STATUS_ABSENT_UNEXCUSED)) * 100
 		);
 	}
 
