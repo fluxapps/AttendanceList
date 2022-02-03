3a037bf64d6864e69b20c23af23cb4de03d460d5
fluxmsadmin <mstuder-persoenlich@fluxlabs.ch>
2022-02-03 19:33:48 +0000

    Update plugin.php

--- plugin.php
+++ plugin.php
@@ -1,8 +1,8 @@
 <?php
 $id = 'xali';
-$version = '1.6.1';
-$ilias_min_version = '5.4.0';
-$ilias_max_version = '6.999';
+$version = '1.7.0';
+$ilias_min_version = '6.0.0';
+$ilias_max_version = '7.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'support@studer-raimann.ch';
 $learning_progress = true;
