2e6e58740fdb123bd1c422c871ff0cf81801de89
fluxms <martin@fluxlabs.ch>
2022-02-17 02:26:18 +0100

    fixes notificaton for plugins

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 $id = 'xali';
 $version = '1.7.0';
-$ilias_min_version = '6.0.0';
+$ilias_min_version = '6.0';
 $ilias_max_version = '7.999';
 $responsible = 'Theodor Truffer';
 $responsible_mail = 'support@studer-raimann.ch';
